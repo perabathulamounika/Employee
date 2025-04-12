3e979c55d8ff8d54227d2488c94cab4bd88c34a1
perabathulamounika <mounikap.perabathula@gmail.com>
2025-04-12 12:40:54 +0530

    first commit

---
+++ PadhuApplication.java
@@ -0,0 +1,13 @@
+package com.example.padhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadhuApplication.class, args);
+	}
+
+}
